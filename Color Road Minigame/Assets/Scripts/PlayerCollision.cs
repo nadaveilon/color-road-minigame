0824d131578c22e9af11ff08390a981ba13fa494
nadaveilon <nadav.vvvvvv@gmail.com>
2021-12-16 11:43:08 +0200

    add trail and blink effect to the ball

--- Color Road Minigame/Assets/Scripts/PlayerCollision.cs
+++ Color Road Minigame/Assets/Scripts/PlayerCollision.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PlayerCollision : MonoBehaviour
 {
@@ -15,5 +16,18 @@ public class PlayerCollision : MonoBehaviour
     public void OnColorChanged(int index, Color color)
     {
         meshRenderer.material.color = color;
+        BlinkWhite();
+    }
+
+    public void OnScoreChanged(string score)
+    {
+        BlinkWhite();
+    }
+
+    private void BlinkWhite()
+    {
+        var original = meshRenderer.material.color;
+        var flashTween = meshRenderer.material.DOColor(Color.white, 0.2f);
+        flashTween.onComplete += () => meshRenderer.material.DOColor(original, 0.2f);
     }
 }
