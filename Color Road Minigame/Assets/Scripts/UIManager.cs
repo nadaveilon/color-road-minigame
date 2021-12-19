6c649cb821d8c75decd888fe34b153bd7b998a38
nadaveilon <nadav.vvvvvv@gmail.com>
2021-12-19 17:33:44 +0200

    move UI manager to main UI canvas

--- Color Road Minigame/Assets/Scripts/UIManager.cs
+++ Color Road Minigame/Assets/Scripts/UIManager.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] private GameObject startScreen;
+    [SerializeField] private GameObject endScreen;
+
     // Update is called once per frame
     void Update()
     {
@@ -13,8 +17,10 @@ public class UIManager : MonoBehaviour
         }
     }
 
-    public void OnAnimationComplete()
+    public void OnStartAnimationComplete()
     {
-        Destroy(gameObject);
+        Destroy(startScreen);
     }
+
+
 }
